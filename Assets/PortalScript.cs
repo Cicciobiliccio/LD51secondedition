2b00e3b1dd7a1876ffd3174ffc05633bb688c026
Cicciobiliccio <s.giovannozzi@gmail.com>
2022-10-02 10:31:25 +0200

    level 1 readyish

---
+++ Assets/PortalScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+
+    public List<GameObject> enemyList = new List<GameObject>();
+    public List<GameObject> portals = new List<GameObject>();
+    public float spawntime;
+    float timer;
+    public int enemyNumber;
+
+    void Start()
+    {
+        foreach (GameObject p in GameObject.FindGameObjectsWithTag("Portal"))
+        {
+            portals.Add(p);
+        }
+    }
+
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = spawntime;
+            for (int a = 0; a < enemyNumber; a++)
+            {
+                GameObject selectedPortal = portals[Random.Range(0, portals.Count)];
+                GameObject selectedEnemy = enemyList[Random.Range(0, enemyList.Count)];
+                Instantiate(selectedEnemy, selectedPortal.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
