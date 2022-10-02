747e4d0550220901d1aa3aa8edf770744890a413
Elise Van Hoof <elise_van_hoof@hotmail.Com>
2022-10-02 08:31:44 +0200

    Update AudioManager.cs

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine.Audio;
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour {
 
@@ -9,15 +10,16 @@ public class AudioManager : MonoBehaviour {
     void Awake() {
         foreach (Sound s in sounds) {
             s.source = gameObject.AddComponent<AudioSource>();
-            s.source.slip = s.clip;
+            s.source.clip = s.clip;
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
         }
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public void Play (string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
+
 }
