bcbcc4378fb28508317cb1b773b0a96a6ea2b6e1
Elise Van Hoof <elise_van_hoof@hotmail.Com>
2022-10-03 19:37:32 +0200

    Update SetVolumeSliders.cs

--- Assets/SetVolumeSliders.cs
+++ Assets/SetVolumeSliders.cs
@@ -14,29 +14,42 @@ public class SetVolumeSliders : MonoBehaviour
     public Slider FxSlider;
 
     public void SetSliders() {
+
         if (PlayerPrefs.HasKey("MasterVolume")) {
             masterSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("MasterVolume");
+            audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
         } else {
-            masterSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("FXVolume");
             audioMixer.GetFloat("MasterVolume", out float MasterVolume);
+
+            audioMixer.SetFloat("MasterVolume", MasterVolume);
+            masterSlider.GetComponent<Slider>().value = MasterVolume;
+
             PlayerPrefs.SetFloat("MasterVolume", MasterVolume);
             PlayerPrefs.Save();
         }
 
         if (PlayerPrefs.HasKey("MusicVolume")) {
             musicSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("MusicVolume");
+            audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
         } else {
-            musicSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("FXVolume");
             audioMixer.GetFloat("MusicVolume", out float MusicVolume);
+
+            audioMixer.SetFloat("MusicVolume", MusicVolume);
+            musicSlider.GetComponent<Slider>().value = MusicVolume;
+
             PlayerPrefs.SetFloat("MusicVolume", MusicVolume);
             PlayerPrefs.Save();
         }
 
         if (PlayerPrefs.HasKey("FXVolume")) {
             FxSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("FXVolume");
+            audioMixer.SetFloat("FXVolume", PlayerPrefs.GetFloat("FXVolume"));
         } else {
-            FxSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("FXVolume");
             audioMixer.GetFloat("FXVolume", out float FXVolume);
+
+            audioMixer.SetFloat("FXVolume", FXVolume);
+            FxSlider.GetComponent<Slider>().value = FXVolume;
+
             PlayerPrefs.SetFloat("FXVolume", FXVolume);
             PlayerPrefs.Save();
         }
